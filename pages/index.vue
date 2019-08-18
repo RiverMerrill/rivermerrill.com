67c248bd4c52043bac0fad582b7325fd6fe1262e
River Merrill <rivermerrill.me@gmail.com>
2019-08-18 16:54:12 -0700

    updated with music shit

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,10 @@
 <template>
   <div class="body">
     <div class="header">
-      <div class="title">
-        <h1 class="text-xs-center display-4">River Merrill</h1>
-        <!-- <div
-          class="line-animation"
-          data-aos="zoom-in"
-          data-aos-duration="3000"
-        ></div> -->
-        <p class="subtitle text-xs-center">Web Developer</p>
+      <div class="title text-xs-center">
+        <nuxt-link to="/music">Music</nuxt-link>
+        <h1 class="display-4">River Merrill</h1>
+        <nuxt-link to="/store">Store</nuxt-link>
       </div>
     </div>
   </div>
@@ -27,37 +23,29 @@ export default {
     VuetifyLogo
   },
   mounted() {
-    this.$anime({
-      targets: '.subtitle',
-      duration: 3000,
-      easing: 'easeInOutQuad',
-      loop: true,
-      direction: 'alternate',
-      keyframes: [
-        { 'border-top': '2px solid red' },
-        { 'border-top': '2px solid grey' },
-        { 'border-right': '2px solid red' },
-        { 'border-right': '2px solid grey' },
-        { 'border-bottom': '2px solid red' },
-        { 'border-bottom': '2px solid grey' },
-        { 'border-left': '2px solid red' },
-        { 'border-left': '2px solid grey' },
-      ]
-
-    })
+  },
+  methods: {
+  },
+  data() {
+    return {
+    }
   }
 }
 </script>
 
 <style scoped>
+.body {
+  height: 100%;
+  overflow: hidden;
+}
 .header {
-  height: 600px;
-  /* background: url("https://www.toptal.com/designers/subtlepatterns/patterns/papyrus.png"); */
+  overflow-x: hidden;
+  height: 100%;
+  background: url("/header.jpg");
+  background-size: cover;
   border-bottom: 1px solid lightgrey;
 }
-.title {
-  margin-top: 20%;
-}
+
 .line-animation {
   border-bottom: 1px solid red;
   width: 40%;
@@ -65,9 +53,35 @@ export default {
   margin-right: auto;
   margin-bottom: 5px;
 }
+
+.title {
+  font-weight: bolder;
+  color: white;
+}
+
+.title h1 {
+  display: inline-block;
+}
+
+.title a {
+  margin: 100px;
+  color: white !important;
+  text-decoration: none;
+}
+.title a:hover {
+  color: pink !important;
+  text-decoration: none;
+}
+
 .subtitle {
   width: 200px;
   margin-left: auto;
   margin-right: auto;
+}
+
+hr {
+  width: 400px;
+  margin: 0 auto 20px auto;
+  border-top: 1px solid red;
 }
 </style>
---
+++ pages/music.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="body">
+    <div class="header text-xs-center">
+      <nuxt-link to="/">Home</nuxt-link>
+      <nuxt-link to="/store">Store</nuxt-link>
+    </div>
+    <v-container>
+      <v-row class="albums">
+        <v-col class="ma-3">
+          <a href="https://song.link/album/us/i/1454553565">
+            <img
+              src="/hardreset.jpg"
+              alt=""
+            >
+          </a>
+        </v-col>
+        <v-col class="ma-3">
+          <a href="https://song.link/album/us/i/1384303675">
+            <img
+              src="/unusual.jpg"
+              alt=""
+            >
+          </a>
+        </v-col>
+        <v-col class="ma-3">
+          <a href="https://song.link/album/us/i/1318531615">
+            <img
+              src="/weatherboy.jpg"
+              alt=""
+            >
+          </a>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.header {
+  padding-bottom: 10%;
+  color: white;
+}
+
+.header a {
+  margin: 200px;
+  font-size: 14pt;
+  color: white !important;
+  text-decoration: none;
+}
+
+.header a:hover {
+  color: #b8eeff !important;
+  text-decoration: none;
+}
+
+.body {
+  height: 100%;
+  background-color: #f9d29d;
+  background-image: linear-gradient(315deg, #f9d29d 0%, #ffd8cb 74%);
+}
+
+.albums v-col img {
+  width: 30%;
+}
+</style>
+
+
